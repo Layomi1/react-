6c5140a82ae29d50bfd2036fce39972d3f00720a
Ayomi <layomilawore@gmail.com>
2024-09-13 15:51:05 +0100

    home page implemented

--- src/components/product-tile/ProductTile.jsx
+++ src/components/product-tile/ProductTile.jsx
@@ -1,6 +1,6 @@
 const ProductTile = ({ product }) => {
   return (
-    <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+    <figure className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
       <div className="h-[180px]">
         <img
           src={product?.image}
@@ -15,7 +15,7 @@ const ProductTile = ({ product }) => {
       <div className="flex items-center justify-between w-full mt-5 ">
         <button className="bg-red-950 text-white rounded-lg font-bold border-2 p-4">Add to Cart</button>
       </div>
-    </div>
+    </figure>
   );
 };
 
