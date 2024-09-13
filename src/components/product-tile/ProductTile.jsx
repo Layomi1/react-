60560ac7549626c9d0ae9311cbea32b505164fc4
Ayomi <layomilawore@gmail.com>
2024-09-13 15:31:34 +0100

    home page implemnted

---
+++ src/components/product-tile/ProductTile.jsx
@@ -0,0 +1,22 @@
+const ProductTile = ({ product }) => {
+  return (
+    <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl">
+      <div className="h-[180px]">
+        <img
+          src={product?.image}
+          alt={product?.title}
+          className="object-cover
+            h-full w-full"
+        />
+      </div>
+      <div >
+        <h1 className="w-40 truncate mt-3 text-gray-700 text-lg font-bold">{product?.title}</h1>
+      </div>
+      <div className="flex items-center justify-between w-full mt-5 ">
+        <button className="bg-red-950 text-white rounded-lg font-bold border-2 p-4">Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductTile;
